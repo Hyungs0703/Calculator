4ccbe657b1f06c2a1625c83ac3e4a763da666461
hyungs0703 <hyungs0703@naver.com>
2024-05-13 14:55:37 +0900

    1-5 배열 생성 및 저장

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,12 @@
+import java.util.Arrays;
 import java.util.Objects;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args)  {
+
+        int[]results = new int[10];
+        int index = 0;
         //스캐너 생성
         Scanner sc = new Scanner(System.in);
         do{
@@ -10,11 +14,12 @@ public class Main {
             int number1 = sc.nextInt();
             System.out.print("두 번째 숫자를 입력하세요 : ");
             int number2 = sc.nextInt();
-            //사칙연산 기호 전달
             System.out.print("사칙 연산 기호를 입력해주세요 : ");
-
             int result = 0;
-            //반복문 사용하여 연산 진행
+
+            //배열 생성, 선언 및 배열안의 선언할 인덱스 증가 생성
+
+
             char operator = sc.next().charAt(0);
             switch(operator){
                 case '+' :
@@ -27,17 +32,25 @@ public class Main {
                     result = number1 * number2;
                     break;
                 case '/':
-                    if(number2 == 0){
-                        System.out.println("두번째 숫자는 0이 입력될 수 없습니다.");
+                    if(number2 != 0){
+                        result= number1 / number2;
                         return;
                     }else{
-                        result = number1 / number2;
+                        System.out.println("두번째 숫자는 0이 입력될 수 없습니다.");
+                        return;
                     }
                 default :
                     System.out.println("사칙연산기호가 잘못 입력되었습니다.");
                     break;
             }
             System.out.println("결과 : " + result);
+            results[index] = result;
+            index++;
+
+            System.out.println("저장 :" + Arrays.toString(results));
+            System.out.println("인덱스 : "+index);
+            //저장 및 인덱스증가
+
             System.out.println("더 계산하겠습니까 ? (exit 입력시 종료)");
         }while(!sc.next().equals("exit"));
     }
